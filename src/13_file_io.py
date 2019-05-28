f5b087bc3a61d901f0ad575535f7c7491a23fb72
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-27 17:51:19 -0700

    completed tasks

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+viewFile = open('foo.txt')
+print(viewFile.read())
+viewFile.close()
+
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +21,7 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+f = open('bar.txt')
+print(f.read())
+f.close()
