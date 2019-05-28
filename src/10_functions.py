3ffda1cc19c6b90d7db25477ed298dea4be327f4
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-27 17:51:10 -0700

    completed tasks

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,6 +2,15 @@
 
 # YOUR CODE HERE
 
+
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+print(is_even(2))
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -10,3 +19,10 @@ num = int(num)
 
 # YOUR CODE HERE
 
+def is_Even(number):
+    if number % 2 == 0:
+        return "Even!"
+    else:
+        return "Odd"
+
+print(is_Even(num))
